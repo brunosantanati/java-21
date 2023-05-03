c22312492530371cd6fe0a36397f9e0203198d32
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-05-03 14:32:49 -0300

    Add link to the podcas episode

--- src/me/brunosantana/sequenced/collection/ExampleSequencedCollection.java
+++ src/me/brunosantana/sequenced/collection/ExampleSequencedCollection.java
@@ -4,6 +4,9 @@ import java.util.LinkedHashSet;
 import java.util.SequencedSet;
 
 /*
+Spotify - Inside Java - Podcast Episode "Sequenced Collections" with Stuart Marks
+https://open.spotify.com/episode/14SIAjU3vXy6s5EFH9bOyW
+
 JEP 431: Sequenced Collections
 https://openjdk.org/jeps/431
 
